c933cc14901edaaa324962ee53b0cd1021ac2301
lakhanismit <smitlakhani2062002@gmail.com>
2023-02-13 14:33:55 +0530

    git through VS code teminal

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
 let h = "hii"
 
+// only for new user install git
+
+// githup ma jai ne ek repository bnavvi
+
+// git ne local ma set krva mate
+// new terminal ma aa code lakhvo
+    // git config --global user.name lakhanismit
+    // git config --global user.name lakhanismit2062002@gmail.com
+
+
+// git ne daily add krva mate
+// new terminal ma aa code lakhvo
+    // git add .
+    // git commit -m "anytext"
+    // git push origin branchname
+
+// first time repository ma push no tahi to
+// github ma je repository ma push krvu hoi ema 
+// …or push an existing repository from the command line ma je 3 link hoi tene terminal ma past krvi
+// pachhi git ne daily add krvano code pacho lakhvano
+
+
 console.log(h);
